28f0937251ade34bbf53f2678f1aeb64c51fac59
Hoàng Nam Vũ <namvu3121@gmail.com>
2025-05-09 21:13:14 +0700

    môtta

---
+++ redux/actions/PhimAction.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const PhimAction = () => {
+  return (
+    <View>
+      <Text>PhimAction</Text>
+    </View>
+  )
+}
+
+export default PhimAction
+
+const styles = StyleSheet.create({})
---
+++ redux/reducers/PhimReducer.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const PhimReducer = () => {
+  return (
+    <View>
+      <Text>PhimReducer</Text>
+    </View>
+  )
+}
+
+export default PhimReducer
+
+const styles = StyleSheet.create({})
