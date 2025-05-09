ac76103271d9ed41b40f07445a804b2eb55b429d
Hoàng Nam Vũ <namvu3121@gmail.com>
2025-05-09 21:10:51 +0700

    first commit

---
+++ redux/store/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+
+export default configureStore(
+    {
+        reducer:{
+            // Add your reducers here
+            // Example: user: userReducer,
+        }
+    }
+)
